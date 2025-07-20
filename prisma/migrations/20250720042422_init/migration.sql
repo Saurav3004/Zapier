ad955e4f4a1a07ace757bc1bd5773fb5680d01d8
Saurav3004 <130168711+Saurav3004@users.noreply.github.com>
2025-07-20 12:28:17 +0530

    created zap

--- prisma/migrations/20250720042422_init/migration.sql
+++ prisma/migrations/20250720042422_init/migration.sql
@@ -11,7 +11,7 @@ CREATE TABLE "User" (
 -- CreateTable
 CREATE TABLE "Zap" (
     "id" TEXT NOT NULL,
-    "zapId" TEXT NOT NULL,
+    "triggerId" TEXT NOT NULL,
 
     CONSTRAINT "Zap_pkey" PRIMARY KEY ("id")
 );
@@ -50,8 +50,28 @@ CREATE TABLE "TriggerAvailable" (
     CONSTRAINT "TriggerAvailable_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "ZapRun" (
+    "id" TEXT NOT NULL,
+    "zapId" TEXT NOT NULL,
+    "metadata" JSONB NOT NULL,
+
+    CONSTRAINT "ZapRun_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "zapRunOutBox" (
+    "id" TEXT NOT NULL,
+    "zapRunId" TEXT NOT NULL,
+
+    CONSTRAINT "zapRunOutBox_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "Trigger_zapId_key" ON "Trigger"("zapId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "zapRunOutBox_zapRunId_key" ON "zapRunOutBox"("zapRunId");
 
 -- AddForeignKey
 ALTER TABLE "Trigger" ADD CONSTRAINT "Trigger_triggerId_fkey" FOREIGN KEY ("triggerId") REFERENCES "TriggerAvailable"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -64,3 +84,9 @@ ALTER TABLE "Action" ADD CONSTRAINT "Action_zapId_fkey" FOREIGN KEY ("zapId") RE
 
 -- AddForeignKey
 ALTER TABLE "Action" ADD CONSTRAINT "Action_actionId_fkey" FOREIGN KEY ("actionId") REFERENCES "ActionAvailable"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ZapRun" ADD CONSTRAINT "ZapRun_zapId_fkey" FOREIGN KEY ("zapId") REFERENCES "Zap"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "zapRunOutBox" ADD CONSTRAINT "zapRunOutBox_zapRunId_fkey" FOREIGN KEY ("zapRunId") REFERENCES "ZapRun"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import express from 'express';
-import { PrismaClient } from './generated/prisma';
+import { PrismaClient } from '@prisma/client';
 const client = new PrismaClient()
 
 const app = express ();
+app.use(express.json())
 
-app.post("/hooks/cache/:userId/:zapId",async (req,res) =>{
+app.post("/hooks/catch/:userId/:zapId",async (req,res) =>{
     const userId = req.params.userId;
     const zapId = req.params.zapId;
     const body = req.body
+    
 
-   await client.$transaction(async tx => {
+   await client.$transaction(async (tx:any) => {
     const run = await tx.zapRun.create({
         data:{
             zapId:zapId,
@@ -23,7 +25,9 @@ app.post("/hooks/cache/:userId/:zapId",async (req,res) =>{
     }
    })
    })
-
-   
-
+   res.json({
+    message:"Webhook Received"
+   })
 })
+
+app.listen(3000);
