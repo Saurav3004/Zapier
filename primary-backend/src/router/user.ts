cde816c8624c3ddfcbeab59647448dc93bce523f
Saurav3004 <130168711+Saurav3004@users.noreply.github.com>
2025-07-26 09:08:46 +0530

    adding prisma client

--- primary-backend/src/router/user.ts
+++ primary-backend/src/router/user.ts
@@ -1,16 +1,28 @@
 import { Router } from 'express'
 import { authMiddleware } from '../middleware';
 import { SignupSchema } from '../types';
+import { prismaClient } from '../db';
 
 const route = Router();
 
-route.post("/signup",(req,res) => {
+route.post("/signup",async (req,res) => {
     const body = req.body.username;
     const parsedData = SignupSchema.safeParse(body);
 
     if(!parsedData.success){
         return res.status(411).json({
             message: "Incorrect inputs"
+        })
+    }
+    const userExists = await prismaClient.user.findFirst({
+        where:{
+            email: parsedData.data.username
+        }
+    })
+
+    if(userExists){
+        return res.status(403).json({
+            message: "User already exists"
         })
     }
 })
